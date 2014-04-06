075329846478650df8f96e11af64281af8477c16
Nesurion <maik.schwan@gmail.com>
2014-04-06 13:53:13 +0200

    Multi path option working

--- TakeOutTheTrash.rb
+++ TakeOutTheTrash.rb
@@ -3,18 +3,34 @@
 ### EDIT THIS ###
 
 #Path to folder to check
-trashbin = "/Users/Nes/Programming/TakeOutTheTrash/testfiles/*.*"
+trashbins = ["/Users/Nes/Programming/TakeOutTheTrash/testfiles/", "/Users/Nes/Programming/TakeOutTheTrash/testfiles2/"]
 
 #Time period to check for, e.g. 2 weeks (in secounds)
 #Format like this: 14*24*60*60 (days*hours*minutes*secounds)
 older_than = 14*24*60*60
 
 ### END OF THE EDIT PART ###
+# .* filtern
+# gesamtspfade erzeugen
+# objekte nach creation time fragen
+# alte daten löschen
 
+absolutePaths = Array.new
+for path in trashbins
+	fileNames = Dir.entries(path)
+	for files in fileNames 
+		if not files.start_with?(".")
+			absolutePaths << path+files
+		end
+	end
+end
+#debug output
+#puts absolutePaths
+#puts
 
-Dir.glob(trashbin) do |item|
-  next if item == '.' or item == '..'
-    if File.ctime(item) < Time.now() - older_than
-      File.delete(item)
-    end
-  end
+for item in absolutePaths
+	if (Time.new.to_i - File.ctime(item).to_i) > older_than
+		File.delete(item) #Delete command - high risk
+	end
+	#puts File.ctime(item).to_i #debug
+end
